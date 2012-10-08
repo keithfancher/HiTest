08a47c78c476f19dfbd11bfe58eeac302d982b48
ktf <not.discostoo@gmail.com>
2012-10-08 14:28:32 -0700

    add known good boilerplate

---
+++ test_data/test_example_module.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+import unittest
+import test_data.example_module
+
+
+class TestAnotherFunction(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+class TestOneMoreFunction(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+class TestSomeFunction(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
